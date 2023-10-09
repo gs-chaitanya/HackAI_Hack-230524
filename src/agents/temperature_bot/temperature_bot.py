3d93bf2844336bd72bb4da6c5a902b0b424a6636
the-dg04 <devgup04@gmail.com>
2023-10-10 04:26:46 +0530

    hehe

--- src/agents/temperature_bot/temperature_bot.py
+++ src/agents/temperature_bot/temperature_bot.py
@@ -32,9 +32,11 @@ async def send_address(ctx:Context):
 
 @temperature_bot.on_message(model=Message)
 async def handle_message(ctx: Context, sender: str, msg: Message):
+    
+    # put city validation here
+
     temperature=get_temperature(msg.message)
     ctx.logger.info(f"recieved message : {msg.message}")
-    # put city validation here
     await ctx.send(sender,Message(message=temperature))
     ctx.logger.info("message sent!")
 
