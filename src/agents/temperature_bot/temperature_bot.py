5cc5e858df4478a90870fa671c8072d0c4bbb19b
StoneZoo <shikhars2003@gmail.com>
2023-10-10 18:11:43 +0530

    Added comments

--- src/agents/temperature_bot/temperature_bot.py
+++ src/agents/temperature_bot/temperature_bot.py
@@ -3,6 +3,13 @@ from uagents.setup import fund_agent_if_low
 import requests
 import os
 from dotenv import load_dotenv
+
+"""
+This is the code for the temperature bot. This bot is responsible for making API calls based on the requests
+sent to it by another bot. This bot sends the temperature info back to the bot making the requests
+"""
+
+
 load_dotenv()
 
 class Message(Model):
@@ -10,19 +17,20 @@ class Message(Model):
 
 ACCESS_KEY = os.getenv('ACCESS_KEY')
 
-def get_temperature( city_id):  
+def get_temperature( city_id):  # Makes api call to get temperature from a given city
     api_url = "http://api.weatherstack.com/current"  
     params = {  
         "query": city_id,
         "access_key":  ACCESS_KEY
     }
-    response = requests.get(api_url, params=params)  
+    response = requests.get(api_url, params=params)  # Make api call
     print(response.status_code)
     data = response.json() 
     temperature = data['current']['temperature']
     return temperature
 
-temperature_bot=Agent(name="Chikorita",seed="Chiokrita",port=42069,endpoint=["http://127.0.0.1:42069/submit"])
+ 
+temperature_bot=Agent(name="Chikorita",seed="Chiokrita",port=42069,endpoint=["http://127.0.0.1:42069/submit"]) # Create the bot
 fund_agent_if_low(temperature_bot.wallet.address())
 
 @temperature_bot.on_event("startup")
@@ -30,11 +38,11 @@ async def send_address(ctx:Context):
     ctx.logger.info(f"address : {ctx.address}")
 
 
-@temperature_bot.on_message(model=Message)
+@temperature_bot.on_message(model=Message) # When a message is received
 async def handle_message(ctx: Context, sender: str, msg: Message):
     temperature=get_temperature(msg.message)
     ctx.logger.info(f"recieved request : {msg.message}")
-    await ctx.send(sender,Message(message=temperature))
+    await ctx.send(sender,Message(message=temperature)) # Send the temperature info to the interface bot
 
 
 if __name__=="__main__":
--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ minT = 0
 CITY = 0
 
 @interface_bot.on_event("startup")
-async def send_address(ctx:Context):
+async def store_params(ctx:Context):  # Store the input given by the user
     ctx.storage.set("MinTemp", minT)
     ctx.storage.set("MaxTemp", maxT)
     ctx.storage.set("CITY", CITY)
@@ -16,16 +16,16 @@ async def send_address(ctx:Context):
 
 if __name__ == "__main__":
 
-    minT, maxT, CITY = Get_Bot_Params()
+    minT, maxT, CITY = Get_Bot_Params() # Take input from user
 
     if(CITY != -1):
-        bureau = Bureau(port=420)
+        bureau = Bureau(endpoint=["http://127.0.0.1:42069/submit"], port=420) # Create a bereau to manage bot interaction
         print(f"Activating {temperature_bot.name}")
         
-        bureau.add(temperature_bot)
+        bureau.add(temperature_bot) 
         print(f"Activating {interface_bot.name}")
-        bureau.add(interface_bot)
-        bureau.run()
+        bureau.add(interface_bot) # Add both bots
+        bureau.run() # Run the bereau
 
 
 
